9403d4683cfcd0b0401c0d9772fcfd5b45c0fed2
Samy-T <samyturculescu@gmail.com>
2020-04-08 20:06:53 +0300

    Adding Client Domain

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientDomain.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Domain.Domain.CreateOrderOp;
+using Domain.Models;
+using Infra.Free;
+using LanguageExt.ClassInstances;
+using static IOExt;
+
+namespace Domain.Domain
+{
+    class ClientDomain
+    {
+        public static IO<CreateOrderResult.ICreateOrderResult> CreateOrder(Restaurant restaurant, int idOrder, List<MenuItem> menuItems)
+           => NewIO<CreateOrderCmd, CreateOrderResult.ICreateOrderResult>(new CreateOrderCmd(restaurant, idOrder, menuItems));
+    }
+}
