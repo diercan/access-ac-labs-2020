0fa97ff318b8fe1a46cf2535f7718500ab9ebeb4
Samy-T <samyturculescu@gmail.com>
2020-04-08 20:03:30 +0300

    Adding Client Model

---
+++ Turculescu_Samuel/Week_2/Domain/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Client
+    {
+        public string Name { get; }
+        public Order Order { get; set; }
+        public Client(string name)
+        {
+            Name = name;
+        }
+    }
+}
