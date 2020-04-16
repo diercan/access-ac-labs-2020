5d69e09bedf3f42bc72fbfe2a5b5bea9c9a882db
Samy-T <samyturculescu@gmail.com>
2020-04-16 16:00:39 +0300

    Adding Employee model

---
+++ Turculescu_Samuel/Week_2/Domain/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class Employee
+    {
+        public string Name { get; }
+
+        public Employee(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
