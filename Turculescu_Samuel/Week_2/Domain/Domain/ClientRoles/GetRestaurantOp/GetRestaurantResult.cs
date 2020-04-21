275a0d0abcfd70e97dbe87f968d589149073ccdd
Samy-T <samyturculescu@gmail.com>
2020-04-22 01:17:09 +0300

    Creating ClientRoles director and adding 1'st client's role - GetRestaurantOp

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/GetRestaurantOp/GetRestaurantResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp.Choices.Attributes;
+using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Domain.Domain.ClientRoles.GetRestaurantOp
+{
+    [AsChoice]
+    public static partial class GetRestaurantResult
+    {
+        public interface IGetRestaurantResult { }
+
+        public class RestaurantGotten : IGetRestaurantResult
+        {
+            public Restaurant Restaurant { get; }
+            public RestaurantGotten(Restaurant restaurant)
+            {
+                Restaurant = restaurant;
+            }
+        }
+
+        public class RestaurantNotGotten : IGetRestaurantResult
+        {
+            public string Reason { get; }
+            public RestaurantNotGotten(string reason)
+            {
+                Reason = reason;
+            }
+        }
+    }
+}
