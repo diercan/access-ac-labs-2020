05cddcfb7c6798d9d0f912316535f93162a39577
Samy-T <samyturculescu@gmail.com>
2020-04-22 16:29:32 +0300

    Adding CreateMenuItemOp in Domain

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/CreateMenuItemOp/CreateMenuItemCmd.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Domain.CreateMenuItemOp
+{
+    public struct CreateMenuItemCmd
+    {
+        public Menu Menu { get; }
+        public string Name { get; }
+        public float Price { get; }
+        public int Quantity { get; }
+        public string Ingredients { get; }
+        public string Allergens { get; }
+
+        public CreateMenuItemCmd(Menu menu, string name, float price, int quantity, string ingredients, string allergens)
+        {
+            Menu = menu;
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Ingredients = ingredients;
+            Allergens = allergens;
+        }
+    }
+}
