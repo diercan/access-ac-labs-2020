387e7315ff1fb8c4c9662d52541c6b5c123b22be
Samy-T <samyturculescu@gmail.com>
2020-04-23 14:28:31 +0300

    Adding GetOrderStatusOp in ClientRoles

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/GetOrderStatusOp/GetOrderStatusResult.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp.Choices.Attributes;
+using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Domain.Domain.ClientRoles.GetOrderStatusOp
+{
+    [AsChoice]
+    public static partial class GetOrderStatusResult
+    {        
+        public interface IGetOrderStatusResult { }
+
+        public class OrderStatus : IGetOrderStatusResult
+        {
+            public string StatusMessage { get; }
+
+            public OrderStatus(string statusMessage)
+            {
+                StatusMessage = statusMessage;
+            }
+        }
+    }
+}
