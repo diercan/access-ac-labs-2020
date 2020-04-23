c74aa211dc6539ccfd902eacbe1af4567f10bdb3
Samy-T <samyturculescu@gmail.com>
2020-04-23 14:27:58 +0300

    Adding PlaceOrderOp in ClientRoles

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/PlaceOrderOp/PlaceOrderCmd.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Domain.ClientRoles.PlaceOrderOp
+{
+    public struct PlaceOrderCmd
+    {
+        public Cart Cart { get; }
+        public Client Client { get; }
+        public uint Tip { get; }
+        public uint TableNumber { get; }
+
+        public PlaceOrderCmd(Client client, Cart cart, uint tip = 0, uint tableNumber = 0)
+        {
+            Client = client;
+            Cart = cart;
+            Tip = tip;
+            TableNumber = tableNumber;
+        }
+
+    }
+}
---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/PlaceOrderOp/PlaceOrderOp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Infra.Free;
+using Infrastructure.Free;
+using LanguageExt;
+using LanguageExt.ClassInstances.Pred;
+using static Domain.Domain.ClientRoles.PlaceOrderOp.PlaceOrderResult;
+
+namespace Domain.Domain.ClientRoles.PlaceOrderOp
+{
+    public class PlaceOrderOp : OpInterpreter<PlaceOrderCmd, PlaceOrderResult.IPlaceOrderResult, Unit>
+    {
+        public override Task<IPlaceOrderResult> Work(PlaceOrderCmd Op, Unit state)
+        {
+            Order Order = new Order(Op.Client.GoToRestaurant.OrderId++);
+
+            return !Exists(Op.TableNumber) ?
+                Task.FromResult<IPlaceOrderResult>(new OrderNotPlaced("Please enter your table number!")) :
+                Task.FromResult<IPlaceOrderResult>(new OrderPlaced(Order));
+        }
+
+        public bool Exists(uint tableNumber)
+        {
+            if(tableNumber > 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
