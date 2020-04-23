0796e89a5ebab030a01909656f9537b88c9013df
Samy-T <samyturculescu@gmail.com>
2020-04-23 14:26:53 +0300

    Adding AddToCartOp in ClientRoles

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/AddToCartOp/AddToCartCmd.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Domain.ClientRoles.AddToCartOp
+{
+    public struct AddToCartCmd
+    {
+        public string SessionId { get; }
+        public Client Client { get; }
+        public MenuItem MenuItem { get; }
+        public uint Quantity { get; }
+        public string SpecialRequests { get; }
+
+        public AddToCartCmd(string sessionId, Client client, MenuItem menuItem, uint quantity, string specialRequests)
+        {
+            SessionId = sessionId;
+            Client = client;
+            MenuItem = menuItem;
+            Quantity = quantity;
+            SpecialRequests = specialRequests;
+        }
+    }
+}
