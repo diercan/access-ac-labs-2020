4d25914a6cafae8b0395e011a1b4f6ae1ec3d5a9
Samy-T <samyturculescu@gmail.com>
2020-04-23 14:26:02 +0300

    Adding Cart and CartItem Models

---
+++ Turculescu_Samuel/Week_2/Domain/Models/CartItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Models
+{
+    public class CartItem
+    {
+        public string Name { get; }
+        public string SpecialRequests { get; set; }
+        public uint Quantity { get; set; }
+        public float Price { get; }
+        public MenuItem MenuItem { get; }
+
+        public CartItem(MenuItem menuItem, string name, string specialRequests, uint quantity, float price)
+        {
+            MenuItem = menuItem;
+            Name = name;
+            SpecialRequests = specialRequests;
+            Quantity = quantity;
+            Price = price;
+        }
+    }
+}
