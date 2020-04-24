2b43ea99872834ef7b8af96ad94fb01e50125a48
Samy-T <samyturculescu@gmail.com>
2020-04-24 04:01:11 +0300

    Adding PayOrderOp in ClientRoles

---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/PayOrderOp/PayOrderOp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Models;
+using Infrastructure.Free;
+using LanguageExt;
+using LanguageExt.ClassInstances.Pred;
+using static Domain.Domain.ClientRoles.PayOrderOp.PayOrderResult;
+
+namespace Domain.Domain.ClientRoles.PayOrderOp
+{
+    public class PayOrderOp : OpInterpreter<PayOrderCmd, PayOrderResult.IPayOrderResult, Unit>
+    {
+        public override Task<IPayOrderResult> Work(PayOrderCmd Op, Unit state)
+        {
+            Op.Client.Order.PaymentStatus = PaymentStatus.Paid;
+
+            return !Exists(Op.Client.CardNumber) ?
+                Task.FromResult<IPayOrderResult>(new PayOrderNotSuccessful("Unsuccessful payment!")) :
+                Task.FromResult<IPayOrderResult>(new PayOrderSuccessful(Op.Client.Order.PaymentStatus));
+        }
+
+        public bool Exists(string CardNumber)
+        {
+            return true;
+        }
+    }
+}
---
+++ Turculescu_Samuel/Week_2/Domain/Domain/ClientRoles/PayOrderOp/PayOrderResult.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CSharp.Choices.Attributes;
+using Domain.Models;
+using System.Threading.Tasks;
+
+namespace Domain.Domain.ClientRoles.PayOrderOp
+{
+    [AsChoice]
+    public static partial class PayOrderResult
+    {
+        public interface IPayOrderResult { }
+
+        public class PayOrderSuccessful : IPayOrderResult 
+        {
+            public PaymentStatus PaymentStatus { get; }
+
+            public PayOrderSuccessful(PaymentStatus paymentStatus)
+            {
+                PaymentStatus = paymentStatus;
+            }
+        }
+
+        public class PayOrderNotSuccessful : IPayOrderResult
+        {
+            public string Reason { get; }
+
+            public PayOrderNotSuccessful(string reason)
+            {
+                Reason = reason;
+            }
+        }
+    }
+}
