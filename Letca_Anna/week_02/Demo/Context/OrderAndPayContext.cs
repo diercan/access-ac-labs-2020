b9a4f0089a8a6f727187ff62360c093db2d4a737
Ana Letca <anadoraletca@gmail.com>
2020-05-01 21:11:51 +0300

    add entities menu, menuitems, employees

--- Letca_Anna/week_02/Demo/Context/OrderAndPayContext.cs
+++ Letca_Anna/week_02/Demo/Context/OrderAndPayContext.cs
@@ -2,7 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Demo;
-using Persistence.EfCore;
 
 namespace Demo.Context
 {
@@ -17,6 +16,9 @@ namespace Demo.Context
         {
         }
 
+        public virtual DbSet<Employees> Employees { get; set; }
+        public virtual DbSet<MenuItems> MenuItems { get; set; }
+        public virtual DbSet<Menus> Menus { get; set; }
         public virtual DbSet<Restaurant> Restaurant { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -30,6 +32,47 @@ namespace Demo.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Employees>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Employer)
+                    .WithMany(p => p.Employees)
+                    .HasForeignKey(d => d.EmployerId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Employees__Emplo__5AEE82B9");
+            });
+
+            modelBuilder.Entity<MenuItems>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.Property(e => e.Price).HasColumnType("smallmoney");
+
+                entity.HasOne(d => d.Menu)
+                    .WithMany(p => p.MenuItems)
+                    .HasForeignKey(d => d.MenuId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__MenuItems__MenuI__60A75C0F");
+            });
+
+            modelBuilder.Entity<Menus>(entity =>
+            {
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50);
+
+                entity.HasOne(d => d.Restaurant)
+                    .WithMany(p => p.Menus)
+                    .HasForeignKey(d => d.RestaurantId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Menus__Restauran__5DCAEF64");
+            });
+
             modelBuilder.Entity<Restaurant>(entity =>
             {
                 entity.Property(e => e.Name)
---
+++ Letca_Anna/week_02/Domain/Entities/MenuItems.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Persistence.EfCore
+{
+    public partial class MenuItems
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int MenuId { get; set; }
+
+        public virtual Menus Menu { get; set; }
+    }
+}
